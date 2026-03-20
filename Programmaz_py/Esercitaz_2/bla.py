cd5410682f24914592cf87b6c6f3d8676a26cbbd
Jenbrn <janice.brun@edu-its.it>
2026-03-20 23:58:44 +0100

    1:0

--- Programmaz_py/Esercitaz_2/bla.py
+++ Programmaz_py/Esercitaz_2/bla.py
@@ -8,50 +8,37 @@
     principale che chieda in input due quantità di tempo e stampi in output quale quantità di
     tempo è maggiore. La funzione deve avere i parametri formali con valori predefiniti.
 """
+
 def calcola_secondi(a: int, b: int, c: int) -> int:
     sec_min = b * 60
     sec_ore = (a * 60) * 60
     return sec_ore + sec_min + c
 
+def estrai_sec():      
+        tempo = input("Inserire una quantità di tempo in formato ore, minuti e secondi usando : come separatore: ")
 
-# tempo = input("Inserire una quantità di tempo in formato ore, minuti e secondi usando : come separatore: ")
+        ore, minuti, secondi = map(int, tempo.split(':'))
+       
+        while minuti > 59 or minuti < 0 or secondi > 59 or secondi < 0:
+            if minuti > 59 or secondi > 59:
+                tempo = input("Non è possibile avere più di 60 min o/e 60 sec. Reinserire l'input: ")
+                ore, minuti, secondi = map(int, tempo.split(':'))
+            elif minuti < 0 or secondi < 0:
+                tempo = input("Non si possono avere valori negativi su min e/o sec: ")
+                ore, minuti, secondi = map(int, tempo.split(':'))
+            
+        return calcola_secondi(ore, minuti, secondi)
+                
+        
 
-# ore, minuti, secondi = tempo.split(':')
-# ore, minuti, secondi = map(int, (ore, minuti, secondi))
-
-# if minuti > 59 or secondi > 59:
-#     tempo = input("Non è possibile avere più di 60 minu o/e 60 sec. Reinserire l'input: ")
-# print(ore, minuti, secondi)
-
-# Risultato = calcola_secondi(ore, minuti, secondi)
-# print(Risultato)
-
-
-verificatore = False
-
-while verificatore != True:
-    tempo = input("Inserire una quantità di tempo in formato ore, minuti e secondi usando : come separatore: ")
-
-    ore, minuti, secondi = tempo.split(':')
-    ore, minuti, secondi = map(int, (ore, minuti, secondi))
-
-    if minuti > 59 or secondi > 59:
-        tempo = input("Non è possibile avere più di 60 minu o/e 60 sec. Reinserire l'input: ")
-
-    elif ore < 0 or minuti < 0 or secondi < 0:
-        tempo = input("impossibile avere valori tempo negativi")     
-    verificatore = True
-
-print(ore, minuti, secondi)
-
-Risultato = calcola_secondi(ore, minuti, secondi)
-print(Risultato)
+def main():
+    primo = estrai_sec()
+    secondo = estrai_sec()
+    return print(max(primo, secondo))
 
 
-
-
-
-
+if __name__ == "__main__":
+    main()
 
 
 
