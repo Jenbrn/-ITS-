707ba92e959815443a78cf0220daff88cfc1345f
Janice Brun <janice.brun@edu-its.it>
2026-04-01 14:47:23 +0200

    python es

--- Fondamenti_python/base_dati/Lez2026_03_25/extra.sql
+++ Fondamenti_python/base_dati/Lez2026_03_25/extra.sql
@@ -1,37 +1,54 @@
+use magazzino;
+
 #1 Selezionare tutti i fornitori.
+select ragione_sociale from fornitori;
  
 #2 Mostrare nome e descrizione di tutte le categorie.
+select nome, descrizione from categorie;
  
 #3 Trovare i prodotti con quantità in stock maggiore di 50.
+ select nome, quantita_stock from prodotti where quantita_stock > 50;
  
 #4 Elencare i fornitori in ordine alfabetico per ragione sociale.
+ select ragione_sociale from fornitori order by ragione_sociale;
  
 #5 Selezionare i prodotti con prezzo inferiore a 20€.
+select nome, prezzo_unitario from prodotti where prezzo_unitario < 20;
  
-#6 Trovare tutti i fornitori che hanno un'email specificata (non NULL).
+#---6 Trovare tutti i fornitori che hanno un'email specificata (non NULL).
+select ragione_sociale, email from fornitori where email != null;
  
 #7 Mostrare i prodotti il cui nome inizia con la lettera 'S'.
+ select nome from prodotti where nome like 's%';
  
 #8 Selezionare i 5 prodotti con la quantità in stock più alta.
+ select nome, quantita_stock from prodotti order by quantita_stock desc limit 5;
  
 #9 Trovare i prodotti con prezzo unitario superiore alla media dei prezzi.
 -- Hint: puoi usare una subquery nella WHERE
+ select nome, prezzo_unitario from prodotti where prezzo_unitario > (select avg(prezzo_unitario) from prodotti);
+ 
  
 #10 Calcolare il valore economico di ogni prodotto (prezzo * quantità)
 --  e mostrare solo quelli con valore superiore a 1000€, ordinati dal più alto.
- 
- 
+ select nome, (prezzo_unitario * quantita_stock) as valore from prodotti where (prezzo_unitario * quantita_stock) > 1000 order by valore desc;
+
 -- ============================================================
 -- PARTE 2 — QUERY CON JOIN (Relazioni tra tabelle)
 -- ============================================================
  
 #11 Visualizzare nome prodotto, nome categoria e città del fornitore
 --  per tutti i prodotti.
+select prodotti.nome, categorie.nome, fornitori.citta from prodotti join categorie on prodotti.id_categoria = categorie.id_categoria
+join fornitori on prodotti.id_fornitore = fornitori.id_fornitore;
  
 #12 Elencare i prodotti forniti da fornitori di 'Roma'.
+select prodotti.nome, fornitori.ragione_sociale from prodotti join fornitori on prodotti.id_fornitore = fornitori.id_fornitore where fornitori.citta = 'Roma';
  
 #13 Mostrare nome prodotto e ragione sociale del fornitore
 --  solo per i prodotti esauriti (stock = 0).
+select prodotti.nome, fornitori.ragione_sociale from prodotti join fornitori on prodotti.id_fornitore = fornitori.id_fornitore where quantita_stock = 0;
+
  
 #14 Trovare i prodotti della categoria 'Abbigliamento'
 --  con prezzo inferiore a 50€.
--- Programmaz_py/Esercitaz_2/es_4.py
+++ Programmaz_py/Esercitaz_2/es_4.py
@@ -2,21 +2,47 @@
 Autore: Janice Brun
 Data: 29/03/2026
 Titolo: Esercizio quattro:
+    4.a Scrivere una funzione che restituisca un valore approssimato di e all'ennesimo termine,
+    dove N è inserito come parametro formale alla funzione. La funzione calcola_e deve richiamare la funzione di calcolo del fattoriale.
+    Scrivere il codice della funzione e il programma principale che la chiama chiedendo in input
+    il numero N.
+
     """
 
 def calcola_e(ter):
+    """Funzione: Calcola l'approssimazione del numero di Nepero
+
+       Parametri : ter -> (int) Numero di termini da unare nella somma 
+    
+       Return : (float) -> il valore approssimato di e 
+    """
     e = 0
     for x in range(0, ter+1):
         e += 1 / fattoriale(x)
     return e
 
 def fattoriale(n):
+    """Funzione : calcola il fattoriale di un numero intero non negatio.
+
+       Parametri : n : (int) > 0
+
+       Return :  (int) -> Il fattoriale ni n (n!)
+    
+    """
     fatt = 1
     for x in range(1, n +1):
         fatt *= x
     return fatt
 
 def main():
+    """Funzione : corpo  principale del programma. Richiede in input
+       un numero int positivo, esegue controllo su intero e valori inseriti per
+       poter fare un cast da str a int. 
+       
+       Return: Calcola e stampa il valoe approssimato del numero di Nepero
+       usando n tentativi
+    
+    """
     termine = input('Inserire il numero di termini N per il calcolo di e: ')
 
     prova = termine.replace('.','')
