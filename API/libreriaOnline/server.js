bad1eb691e9d34eb6439dce07ee7da8b5b7bfead
Janice Brun <janice.brun@edu-its.it>
2026-04-15 12:45:46 +0200

    rest

--- API/libreriaOnline/server.js
+++ API/libreriaOnline/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 const PORT = 3000;
 const BASE_PATH = "/api/v1";
 
- 
+
 //ar catalogo = catalogoLibri;
 //var clienti = clienteFile;
 var catalogo = [...catalogoLibri];
@@ -48,7 +48,7 @@ app.get(`${BASE_PATH}/libri/search`, (req, res) =>{
     //recupero il parametro dall'url + pulizia
     let titoloParam = (req.query.titolo || "").toString().trim().toLowerCase();
 
-    //se non c'è il parametro, restituisco stato 400(Bad Req)
+    //se non c'è il parametro o è str vuota, restituisco stato 400(Bad Req)
     if (!titoloParam) return res.status(400).json({ error: "Parametro 'titolo' errato"});
 
     let results = catalogo.findLast(libro => libro.titolo.toLocaleLowerCase().includes(titoloParam));
@@ -66,9 +66,9 @@ app.get(`${BASE_PATH}/libri/:id`, (req, res) => {
     let libroTrovato = catalogo.find(libro => libro.id === idParam);
 
     if (!libroTrovato) {
-        return res.status(404).json({ error: "Libro non trovato"});
+        res.status(404).json({ error: "Libro non trovato"});
     } else {
-        return res.json(libroTrovato);
+        res.json(libroTrovato);
     }
 
 });
@@ -77,17 +77,152 @@ app.get(`${BASE_PATH}/libri/:id`, (req, res) => {
 app.post(`${BASE_PATH}/libri`, (req, res) => {
     const nuovoLibro = req.body;
 
-    if (!nuovoLibro) {
-        return res.status(400).json({ error: "Nessun dato fornito" });
+    if (!validaLibro(nuovoLibro) ) {
+        res.status(400).json({ error: "Nessun dato fornito" });
+        return;
     }
+    
+    // assegno un id univoco; normalmente questa operazione la farebbe il database sottostante
+    const maxId = catalogo.map(item => item.id).reduce((a, b) => Math.max(a, b), 0);
+    nuovoLibro.id = maxId + 1;
 
+    
     catalogo.push(nuovoLibro);
     return res.status(201).json({ message: "Libro aggiuntocon successo" });
+});
+
+
+app.delete(`${BASE_PATH}/libri/:id`, (req, res) => {
+    //recupero id del libro da eliminare dal path e lo converto in numero
+    const idParam = parseInt(req.params.id);
+
+    //cerco nelcatalogo index elemento corrispondente
+    
 
 
 });
 
+app.put(`${BASE_PATH}/libri/:id`, (req, res) => {
+    const idParam = parseInt(req.params.id);
+    //recupero il libro aggiornato dalla request
+    let libroAggiornato = req.body;
+    
+    //controllo che il libro sia valido
+    if (!validaLibro(libroAggiornato)) {
+        return res.status(400).json({error: 'libro non valido'});
+        
+    }    
+    //recupero indice libro da aggiornare
+    // normalmente basta passare l'id ad db per aggiornare il record
+    const index = catalogo.findIndex(libro => libro.id === idParam);
+    ///se non trova l'elemento restituisce errore
+    if (index === -1) {
+        return res.status(404).json({ error : 'Libro non trovato'});
+        ;
+    }
+
+    let libroDaAggiornare = catalogo[index];
+
+    /* l'uso dello spread operator (...) consente di creare un nuovo oggetto che
+    aggiorna le proprieta esistenti di libroDaAggiornare con quelle contenute in libroAggiornato
+    */
+    catalogo[index] = {...libroDaAggiornare, ...libroAggiornato};
+
+    res.json({ message: 'Libro aggiornato con successo'});
+});
 
 
-//app.get(`$`)
-//app.get(`$`)
+app.delete(`${BASE_PATH}/libri/:id`, (req, res) => {
+    // recupero l'ID del libro da eliminare dal path e lo converto in numero
+    const idParam = parseInt(req.params.id);
+    console.log("riga 138");
+
+    // cerco nel catalogo il libro corrispondente usando come parametro l'ID
+    const index = catalogo.findIndex(libro => libro.id === idParam);
+
+    console.log("riga 142");
+    if (index === -1) {
+        return res.status(404).json({ error: 'Libro non trovato' });
+    }
+    
+    console.log("riga 147");
+    catalogo.splice(index, 1);
+    res.json({ message: 'Libro eliminato con successo' });
+
+    // 
+});
+
+//clienti get con id
+app.get(`${BASE_PATH}/clienti/:id`, (req, res) => {
+    let idParam = parseInt(req.params.id);
+
+    let clienteTrovato = clienti.find(cliente => cliente.id === idParam);
+
+    if (!clienteTrovato) {
+        res.status(404).json({ error: "Cliente non trovato"});
+    } else {
+        res.json(clienteTrovato);
+    }
+
+});
+
+//post clienti
+
+app.post(`${BASE_PATH}/clienti`, (req, res) => {
+    const nuovoCliente = req.body;
+
+    if (!validaCliente(nuovoCliente) ) {
+        res.status(400).json({ error: "Nessun dato fornito" });
+        return;
+    }
+    
+    // assegno un id univoco; normalmente questa operazione la farebbe il database sottostante
+    const maxId = clienti.map(item => item.id).reduce((a, b) => Math.max(a, b), 0);
+    nuovoCliente.id = maxId + 1;
+
+    
+    clienti.push(nuovoCliente);
+    return res.status(201).json({ message: "Cliente aggiunto con successo" });
+});
+
+// delete cliente
+app.delete(`${BASE_PATH}/clienti/:id`, (req, res) => {
+    
+    const idParam = parseInt(req.params.id);
+    //console.log("riga 138");
+
+    // cerco nel catalogo il libro corrispondente usando come parametro l'ID
+    const index = clienti.findIndex(cliente => cliente.id === idParam);
+
+    //console.log("riga 142");
+    if (index === -1) {
+        return res.status(404).json({ error: 'Cliente non trovato' });
+    }
+    
+    //console.log("riga 147");
+    clienti.splice(index, 1);
+    res.json({ message: 'Cliente eliminato con successo' });
+
+    // 
+});
+
+
+
+function validaLibro(libro) {
+    //controlliamo che il libro esista e che abbia almeno titolo, autore e editore
+    if (libro && libro.titolo && libro.autore && libro.editore){
+        return true;
+    } else {
+        return false;
+    }    
+    };
+
+function validaCliente(cliente) {
+    //controlliamo che il libro esista e che abbia almeno titolo, autore e editore
+    if (cliente && cliente.nome && cliente.cognome && cliente.eta){
+        return true;
+    } else {
+        return false;
+    }    
+    };
+
